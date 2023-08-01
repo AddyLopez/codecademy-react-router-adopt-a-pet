6600e510f177c2aed9f8c7374699612e0425c184
AddyLopez <101231208+AddyLopez@users.noreply.github.com>
2023-08-01 00:01:58 -0600

    Employ the useNavigate hook

--- src/pages/petDetailsNotFound/index.js
+++ src/pages/petDetailsNotFound/index.js
@@ -3,8 +3,7 @@ import React from "react";
 import { useNavigate } from "react-router-dom";
 
 const PetDetailsNotFound = () => {
-  // get the navigate function from useNavigate
-  const navigate = "REPLACE ME";
+  const navigate = useNavigate();
 
   const goHome = () => {
     // Go home!
