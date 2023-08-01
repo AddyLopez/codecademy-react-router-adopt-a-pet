e8770dce25741ba73d55894b5c26c1a4158f3d74
AddyLopez <101231208+AddyLopez@users.noreply.github.com>
2023-07-31 23:55:40 -0600

    Import Navigate component

--- src/pages/detail/index.js
+++ src/pages/detail/index.js
@@ -2,8 +2,7 @@ import React, { useEffect, useState } from "react";
 import { getPetDetails } from "../../api/petfinder";
 import Hero from "../../components/hero";
 
-import { useParams } from "react-router-dom";
-// Import Navigate
+import { Naviate, useParams } from "react-router-dom";
 
 const PetDetailsPage = () => {
   const [data, setData] = useState();
