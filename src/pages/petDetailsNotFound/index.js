8d064a9ddbc80a9b84f2bc77ed5437e594bdb8ad
AddyLopez <101231208+AddyLopez@users.noreply.github.com>
2023-08-01 00:09:11 -0600

    Fix text alignment on petDetailsNotFound paragraph

--- src/pages/petDetailsNotFound/index.js
+++ src/pages/petDetailsNotFound/index.js
@@ -12,7 +12,7 @@ const PetDetailsNotFound = () => {
   return (
     <main className="page">
       <h3>404: Who let the dogs out?</h3>
-      <p>
+      <p className="not-found">
         Sorry, but the details for this pet have not been uploaded by the
         shelter yet. Check back later!
       </p>
