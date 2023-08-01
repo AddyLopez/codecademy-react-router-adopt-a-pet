3b407b1e1bf3132df393ca2ca0d7f876bd651692
AddyLopez <101231208+AddyLopez@users.noreply.github.com>
2023-07-31 22:30:05 -0600

    Import useParams

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import { getPets } from "../../api/petfinder";
 import Hero from "../../components/hero";
 
-// import useParams
+import { useParams } from "react-router-dom";
 // import Link
 
 const HomePage = () => {
