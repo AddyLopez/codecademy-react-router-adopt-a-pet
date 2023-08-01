9ef2a6f3e18679dca7c7f0bc88b6f9affb884fb6
AddyLopez <101231208+AddyLopez@users.noreply.github.com>
2023-07-31 21:58:34 -0600

    Import ReactProvider

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import HomePage from './pages/home';
-import SearchPage from './pages/search';
-import PetDetailsPage from './pages/detail';
-import PetDetailsNotFound from './pages/petDetailsNotFound';
-import Root from './components/root';
+import HomePage from "./pages/home";
+import SearchPage from "./pages/search";
+import PetDetailsPage from "./pages/detail";
+import PetDetailsNotFound from "./pages/petDetailsNotFound";
+import Root from "./components/root";
 
 // Add react-router-dom imports
+import { ReactProvider } from "react-router-dom";
 
 // create router with JSX Route elements
 const appRouter = "REPLACE ME";
